ac8a9699b259b0abaeba6b8ab04a492c6ee86588
Stephen Rayner <srayner@neworbit.co.uk>
2019-08-30 09:05:52 +0100

    looking into plotting options

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.ML;
 using Microsoft.ML.Data;
+using PLplot;
 
 class Program
 {
@@ -18,29 +19,52 @@ class Program
 
     static void Main(string[] args)
     {
-        MLContext mlContext = new MLContext();
+        var mlContext = new MLContext();
+        {
+            // 1. Import or create training data
+            var trainingData = mlContext.Data.LoadFromEnumerable(
+                new HouseData[]
+                {
+                new HouseData() { Size = 1.1F, Price = 1.2F },
+                new HouseData() { Size = 1.9F, Price = 2.3F },
+                new HouseData() { Size = 2.8F, Price = 3.0F },
+                new HouseData() { Size = 3.4F, Price = 3.7F }
+                });
 
-        // 1. Import or create training data
-        HouseData[] houseData = {
-               new HouseData() { Size = 1.1F, Price = 1.2F },
-               new HouseData() { Size = 1.9F, Price = 2.3F },
-               new HouseData() { Size = 2.8F, Price = 3.0F },
-               new HouseData() { Size = 3.4F, Price = 3.7F } };
-        IDataView trainingData = mlContext.Data.LoadFromEnumerable(houseData);
+            // 2. Specify data preparation and model training pipeline
+            var pipeline = mlContext.Transforms.Concatenate("Features", new[] { "Size" })
+                .Append(mlContext.Regression.Trainers.Sdca(
+                    labelColumnName: "Price",
+                    maximumNumberOfIterations: 100));
 
-        // 2. Specify data preparation and model training pipeline
-        var pipeline = mlContext.Transforms.Concatenate("Features", new[] { "Size" })
-            .Append(mlContext.Regression.Trainers.Sdca(labelColumnName: "Price", maximumNumberOfIterations: 100));
+            // 3. Train model
+            var model = pipeline.Fit(trainingData);
 
-        // 3. Train model
-        var model = pipeline.Fit(trainingData);
+            // 4. Make a prediction with test data
+            var testingData = new HouseData() { Size = 2.5F };
+            var price = mlContext.Model.CreatePredictionEngine<HouseData, Prediction>(model)
+                .Predict(testingData);
 
-        // 4. Make a prediction
-        var size = new HouseData() { Size = 2.5F };
-        var price = mlContext.Model.CreatePredictionEngine<HouseData, Prediction>(model).Predict(size);
+            Console.WriteLine($"Predicted price for size: {testingData.Size * 1000} sq ft= {price.Price * 100:C}k");
 
-        Console.WriteLine($"Predicted price for size: {size.Size * 1000} sq ft= {price.Price * 100:C}k");
+            // Predicted price for size: 2500 sq ft= $261.98k
+        }
 
-        // Predicted price for size: 2500 sq ft= $261.98k
+        // 5. Visualize our data
+        var pl = new PLStream();
+        pl.sdev("svg");
+        pl.sfnam("linear-regression.svg");
+        pl.spal0("cmap0_alternate.pal");
+        pl.init();
+
+        const int xMin = 0;
+        const int xMax = 1000;
+        const int yMin = -1;
+        const int yMax = 1;
+        pl.env(xMin, xMax, yMin, yMax, AxesScale.Independent, AxisBox.BoxTicksLabelsAxes);
+        pl.schr(0, 1.25);
+        pl.lab(nameof(HouseData.Size), nameof(HouseData.Price), "Linear regression plot of house price for size");
+        pl.eop();
+
     }
 }
--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 An example of linear regression is how house prices change with size. There are many other factors that contribute to the house price, but one of those features is the size of the house. We would expect a positive relationship between the house price and size. In other words the house price increases with the size.
 
+```
+dotnet add package Microsoft.ML
+```
+
+This is a very simple model as it only has one feature and one key, normally there will be many features contributing to the key. To visualize this plot we can use `PLplot` which is a cross platform plotting library `PLPlotNet` offers binding for .Net.
+
+PlPlot is already included on Windows as a part of [Microsoft Visual C++ Redistributable 2017](https://support.microsoft.com/en-us/help/2977003/the-latest-supported-visual-c-downloads).
+
+```
+dotnet add package PLplot
+```
